a739a04a8bdfe64b91dc6d807a3d6e2c32f2c8fc
Dayly Yurisel Orellana Orellana <dorellanao4@miumg.edu.gt>
2026-02-19 23:34:56 -0600

    libreria lista e instalada

---
+++ umg.edu.gt.data-structure.stack/src/main/java/umg/edu/gt/data_structure/stack/App.java
@@ -0,0 +1,13 @@
+package umg.edu.gt.data_structure.stack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
